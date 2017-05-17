1da52da95904a981824552d2cf78881698d25ff8
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-05-17 09:57:33 -0700

    initial commit

---
+++ ClassLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLib
+{
+    public static class Class1
+    {
+        public static string GetMessage()
+        {
+            return "This message came from the library";
+        }
+    }
+}
